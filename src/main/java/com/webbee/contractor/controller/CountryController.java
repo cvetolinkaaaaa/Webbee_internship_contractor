fa0d8b954d6b7c03aaed23ca6b6d3841d1ab5505
cvetolinkaaaaa <c.evseeva05@mail.ru>
2025-07-06 12:58:26 +0300

    docs: add swagger

--- src/main/java/com/webbee/contractor/controller/CountryController.java
+++ src/main/java/com/webbee/contractor/controller/CountryController.java
@@ -2,6 +2,8 @@ package com.webbee.contractor.controller;
 
 import com.webbee.contractor.dto.CountryDto;
 import com.webbee.contractor.service.CountryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/country")
+@Tag(name = "Country", description = "Country API")
 public class CountryController {
 
     private final CountryService countryService;
@@ -25,21 +28,25 @@ public class CountryController {
     }
 
     @GetMapping("/all")
+    @Operation(summary = "Получить все страны", description = "Возвращает список всех активных стран")
     public List<CountryDto> getAll() {
         return countryService.getAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получить страну по id", description = "Возвращает страну по указанному id")
     public CountryDto getById(@PathVariable String id) {
         return countryService.getById(id);
     }
 
     @PutMapping("/save")
+    @Operation(summary = "Сохранить страну", description = "Сохраняет страну или изменяет существующую")
     public void save(@RequestBody CountryDto countryDto) {
         countryService.save(countryDto);
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Удалить страну", description = "Производит логическое удаление страны")
     public void delete(@PathVariable String id) {
         countryService.delete(id);
     }

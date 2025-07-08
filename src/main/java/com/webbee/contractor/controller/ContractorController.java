1e3de4d478a64a370d6dafc5318128a90395c638
Evseeva Tsvetolina <c.evseeva05@mail.ru>
2025-07-09 01:14:11 +0300

    style: fixed code style

--- src/main/java/com/webbee/contractor/controller/ContractorController.java
+++ src/main/java/com/webbee/contractor/controller/ContractorController.java
@@ -4,8 +4,13 @@ import com.webbee.contractor.dto.ContractorDto;
 import com.webbee.contractor.service.ContractorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
@@ -44,4 +49,5 @@ public class ContractorController {
         contractorService.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
+
 }
